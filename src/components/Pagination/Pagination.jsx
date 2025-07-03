b24981bd0a266b3ce9a6c9d08c08210b4be30b45
Sunil Kulkarni <sunil.kulkarni@valtech.com>
2025-07-03 18:54:12 +0530

    updated to folderstructure

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Pagination = (props) => {
+    const {} = props;
+    return <div></div>;
+}
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Pagination } from "./Pagination/Pagination";
+export { InfiniteScroll } from "./InfiniteScroll/InfiniteScroll";
+export { ProgressBar } from "./ProgressBar/ProgressBar";
+export { StarRating } from "./Rating/StarRating";
+export { AutocompleteSearch } from "./AutocompleteSearch/AutocompleteSearch";
